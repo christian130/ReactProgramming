713851fa36e5a6dce12051d5e339919bd128cf9b
unknown <christian130@gmail.com>
2020-01-16 15:14:24 -0400

    first commit

--- app/src/main/java/com/christian130/rxjava/MainActivity.java
+++ app/src/main/java/com/christian130/rxjava/MainActivity.java
@@ -3,6 +3,18 @@ package com.christian130.rxjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.christian130.rxjava.models.Task;
+import com.christian130.rxjava.utils.DataSource;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Observable<Task> taskObservable = Observable
+                .fromIterable(DataSource.createTaskList())
+                .subscribeOn(Schedulers.io())
+                .filter(new Predicate<Task>() {
+                    @Override
+                    public boolean test(Task task) throws Exception {
+                        Log.d("filtro","a filter was added so the current task object is"+ Thread.currentThread().getName());
+                        try {
+                            Thread.sleep(3000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return task.isComplete();
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread());
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d("obSubscribeMethod","called on subscribe with Disposable object");
+
+            }
+
+            @Override
+            public void onNext(Task task) {
+                Log.d("OnNext","called from OnNext()"+Thread.currentThread().getName());
+                Log.d("OnNext: ","called from OnNext() with a POJO Task named task"+task.getDescription());
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("OnError()",Log.getStackTraceString(e));
+
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d("everything","everything is completed");
+
+            }
+        });
     }
 }
