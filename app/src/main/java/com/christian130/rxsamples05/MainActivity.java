929ea6f0206bc686247d6743b6637a17caf0bf55
unknown <christian130@gmail.com>
2020-02-07 16:09:10 -0400

    this is a TextView created dinamically and used the subcribe method OnNext() to draw itself... also it's added a simple filter in the Observable creation .fromIterable(List<myGeneric>)// este es un TextView creado dinamicamente y usado por el metodo del suscriptor llamado OnNext() y tambien se aplica un filter para que solo entren los elementos de la lista del DTO inicializado que tiene el booleano en true

--- app/src/main/java/com/christian130/rxsamples05/MainActivity.java
+++ app/src/main/java/com/christian130/rxsamples05/MainActivity.java
@@ -1,12 +1,22 @@
 package com.christian130.rxsamples05;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.app.ActionBar;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.christian130.rxsamples05.DTO.Tarea;
 import com.christian130.rxsamples05.model.OrigenDelRecurso;
+
+import org.w3c.dom.Text;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
@@ -17,11 +27,21 @@ import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Context ctx;
+    private ConstraintSet constraintSet;
+    private ConstraintLayout constraintLayout;
+    private AtomicInteger counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        constraintLayout = (ConstraintLayout) findViewById(R.id.myconstrain);
+
+        constraintSet = new ConstraintSet();
+        constraintSet.clone(constraintLayout);
+        ctx=this;
+        counter = new AtomicInteger(60);
+
         Observable<Tarea> tareaObservable = Observable
                 .fromIterable(OrigenDelRecurso.crearLista())
                 .filter(new Predicate<Tarea>() {
@@ -40,8 +60,27 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNext(Tarea tarea) {
-                Log.d("tarea","esta tarea tiene el booleano en true");
+                int numberphile=counter.addAndGet(60);
+                /*TextView nova = new TextView(ctx);
+                nova.setText("this is the text");
+                nova.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
+                nova.setId(View.generateViewId());
+                ConstraintSet constraintSet = new ConstraintSet();
+                constraintSet.clone(constraintLayout);
+                constraintSet.applyTo(constraintLayout);*/
 
+                ConstraintLayout layout = (ConstraintLayout)findViewById(R.id.myconstrain);
+                ConstraintSet set = new ConstraintSet();
+
+                TextView view = new TextView(ctx);
+                view.setText(tarea.getDescripcion()+ " this TextView has been created  dinamically");
+                view.setId(View.generateViewId());
+                layout.addView(view,0);
+                set.clone(layout);
+                set.connect(view.getId(), ConstraintSet.TOP, layout.getId(), ConstraintSet.TOP, numberphile);
+                set.applyTo(layout);
+
+                Log.d("tarea","esta tarea tiene el booleano en true");
             }
 
             @Override
