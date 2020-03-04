f6e49287f30daaf93a33c1e0e8a3a1cba4a06974
unknown <christian130@gmail.com>
2020-03-04 09:04:46 -0400

    the .gitignore has been fixed in order to not to take changs into files from the folder /.idea and /build this is the very same first example but with an intermediate operator named concatMap... which was actually does this operator is requesting one by one instead all of once in a bunch... when we say the order is not respected is according to the request to the server... to see this please see in android studio the logcat that i 'inyected' the OkHttp_client + Interceptor(Logger) and you can see that the request is made one by one... in this scenario

--- app/src/main/java/com/christian130/rxsamples/MainActivity.java
+++ app/src/main/java/com/christian130/rxsamples/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         getPostsObservable()
                 .subscribeOn(Schedulers.io())
-                .flatMap(new Function<Post, ObservableSource<Post>>() {
+                .concatMap(new Function<Post, ObservableSource<Post>>() {
                     @Override
                     public ObservableSource<Post> apply(Post post) throws Exception {
                         return getCommentsObservable(post);
