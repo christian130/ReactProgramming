da9f21b6c72e07bd7fb4088aa042eb546ec376df
unknown <christian130@gmail.com>
2020-03-04 16:12:59 -0400

    new one

--- app/src/main/java/com/christian130/rxsamples/models/adapters/MainRecylerAdapter.java
+++ app/src/main/java/com/christian130/rxsamples/models/adapters/MainRecylerAdapter.java
@@ -33,20 +33,26 @@ import com.christian130.rxsamples.models.DTO.Post;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainRecylerAdapter extends RecyclerView.Adapter<MainRecylerAdapter.MyAnotherClass> {
+public class MainRecylerAdapter extends RecyclerView.Adapter<MainRecylerAdapter.MyViewHolder> {
     private static final String TAG = "RecyclerAdapter";
 
     private List<Post> posts = new ArrayList<>();
+    private OnPostClickListener onPostClickListener;
+
+    public MainRecylerAdapter(OnPostClickListener onPostClickListener) {
+        this.onPostClickListener = onPostClickListener;
+    }
 
     @NonNull
     @Override
-    public MyAnotherClass onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_post_list_item, null, false);
-        return new MyAnotherClass(view);
+        return new MyViewHolder(view, onPostClickListener);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyAnotherClass holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
         holder.bind(posts.get(position));
     }
 
@@ -55,52 +61,46 @@ public class MainRecylerAdapter extends RecyclerView.Adapter<MainRecylerAdapter.
         return posts.size();
     }
 
-    public void setPosts(List<Post> posts) {
+    public void setPosts(List<Post> posts){
         this.posts = posts;
         notifyDataSetChanged();
     }
 
-    public void updatePost(Post post) {
+    public void updatePost(Post post){
         posts.set(posts.indexOf(post), post);
         notifyItemChanged(posts.indexOf(post));
     }
+
     public List<Post> getPosts(){
         return posts;
     }
 
-    public class MyAnotherClass extends RecyclerView.ViewHolder {
-        TextView title, numComments;
-        ProgressBar progressBar;
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        public MyAnotherClass(@NonNull View itemView) {
+        OnPostClickListener onPostClickListener;
+        TextView title;
+
+        public MyViewHolder(@NonNull View itemView, OnPostClickListener onPostClickListener) {
             super(itemView);
             title = itemView.findViewById(R.id.title);
-            numComments = itemView.findViewById(R.id.num_comments);
-            progressBar = itemView.findViewById(R.id.progress_bar);
+
+            this.onPostClickListener = onPostClickListener;
+
+            itemView.setOnClickListener(this);
         }
+
         public void bind(Post post){
             title.setText(post.getTitle());
 
-            if(post.getComments() == null){
-                showProgressBar(true);
-                numComments.setText("");
-            }
-            else{
-                showProgressBar(false);
-                numComments.setText(String.valueOf(post.getComments().size()));
-            }
-
-            
-            
         }
 
-        private void showProgressBar(boolean b) {
-            if(b) {
-                progressBar.setVisibility(View.VISIBLE);
-            }
-            else{
-                progressBar.setVisibility(View.GONE);
-            }
+        @Override
+        public void onClick(View v) {
+            onPostClickListener.onPostClick(getAdapterPosition());
         }
+    }
+
+    public interface OnPostClickListener{
+        void onPostClick(int position);
     }
 }
--- app/src/main/java/com/christian130/rxsamples/models/irequest/ImplSrvIntAll.java
+++ app/src/main/java/com/christian130/rxsamples/models/irequest/ImplSrvIntAll.java
@@ -31,6 +31,8 @@ public interface ImplSrvIntAll {
     @GET("posts")
     Observable<List<Post>> getPosts();
 
-    @GET("post/{id}/comments")
-    Observable<List<Comment>> getAllComments(@Path("id") int id);
+    @GET("posts/{id}")
+    Observable<Post> getPost(
+            @Path("id") int id
+    );
 }
